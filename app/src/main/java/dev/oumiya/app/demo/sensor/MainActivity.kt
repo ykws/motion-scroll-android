7b0c7ca02b49b5a15ac06fedcaaaef6c18c5faeb
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2025-08-04 15:38:48 +0900

    Unused import

--- app/src/main/java/dev/oumiya/app/demo/sensor/MainActivity.kt
+++ app/src/main/java/dev/oumiya/app/demo/sensor/MainActivity.kt
@@ -1,5 +1,6 @@
 package dev.oumiya.app.demo.sensor
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
